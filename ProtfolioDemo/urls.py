9a067f43da6080a553a09ca7b3cd63dd656e7e6b
sharminsayed <sharminsayedshammi@gmail.com>
2020-01-27 21:27:09 +0600

    Static Fixed

--- ProtfolioDemo/urls.py
+++ ProtfolioDemo/urls.py
@@ -13,18 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
-
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('MainApp.urls')),
-    path('index/',include('MainApp.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', include('MainApp.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
