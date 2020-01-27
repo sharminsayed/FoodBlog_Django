a73554b6a9aa3a09f60513e84038f00e27de0bd4
sharminsayed <sharminsayedshammi@gmail.com>
2020-01-27 21:31:00 +0600

    Static Fixed

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
 
     path('',views.homepage,name='homepage'),
-    path('index/',views.homepage,name='homepage'),
+    #  path('index/',views.homepage,name='homepage'),
     path('about/', views.about, name='about'),
     path('contact/',views.contact,name='contact'),
     path('recipes/',views.recipes,name='recipes'),
